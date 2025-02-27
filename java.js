40f21a739d35aef7666b69119ad8c911799efcce
doniyor209 <doniyortoshev123@gmail.com>
2025-02-27 17:32:24 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+alert("Hozir biz random son chiqazamiz: 👍");
+let randomson=Math.floor(Math.random()*90+10);
+alert("Random son: "+randomson);
+confirm("Random sonni 2ga ko`paytirib, kubini topmoqchimiz")
+alert(`Random son:${randomson}\n Random sonni 2ga ko'paytirdik: ${randomson*2}\n Random sonning kubi: ${randomson**3}`);
